832711c7b07917bb7d292339a23917b2debc5fce
marb61a <marb61a@gmail.com>
2019-03-04 12:26:17 +0000

    added initial blog page and updated layout and config page to change author and default name

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
@@ -15,10 +8,11 @@ import "./layout.css"
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
-      query SiteTitleQuery {
+      {
         site {
           siteMetadata {
-            title
+            title,
+            author
           }
         }
       }
@@ -36,9 +30,8 @@ const Layout = ({ children }) => (
         >
           <main>{children}</main>
           <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            © {new Date().getFullYear()}, Built by 
+            {data.site.siteMetadata.author}
           </footer>
         </div>
       </>
---
+++ src/pages/blog.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Static, graphql} from 'gatsby';
+
+import Layout from '../components/layout';
+
+export default () => (
+  <Layout>
+    <div>
+      <h1
+        style={{display: 'inlineBlock', borderBottom: '1px solid'}}
+      >
+        Gatsby Couture blog
+      </h1>
+    </div>
+  </Layout>
+)
