0d8fb276aef732af95bbc64caa295723b3c87f4e
marb61a <marb61a@gmail.com>
2019-03-10 11:25:00 +0000

    converting header to class component

--- src/components/header.js
+++ src/components/header.js
@@ -2,41 +2,42 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+const isActive = ({ isCurent }) => {
+  return { className: isCurrent ? 'active' : 'navlink' }
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
+const NavLink = props => <Link getProps={isActive} {...props} />
+
+class Header extends React.Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    const { siteTitle } = this.props
+
+    return(
+      <div
+        style={{
+          background: `rebeccapurple`,
+          marginBottom: `1.45rem`,
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `1.45rem 1.0875rem`,
+          }}
+        >
+        
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Header
