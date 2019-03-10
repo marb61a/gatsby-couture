420aee32075c8ff1386b9bb988fe923209873cda
marb61a <marb61a@gmail.com>
2019-03-10 11:44:10 +0000

    Added initial page 3 page and moved posts folder up a level

---
+++ src/pages/page-3.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { graphql, StaticQuery, Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const getImageData = graphql `
+  {
+
+  }
+`;
+
+export default () => (
+  <Layout>
+    <h1>Hello from Page 3!</h1>
+    <h3>Image File Data</h3>
+    <StaticQuery
+      query={getImageData}
+      render={}
+    />
+    <Link to="/page-2">Go to page 2</Link>
+  </Layout>
+)
