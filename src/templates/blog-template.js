586bd6fab7f1a193870efd32e65bc44a7c30267c
marb61a <marb61a@gmail.com>
2019-03-11 12:34:54 +0000

    updated blog template

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -19,8 +19,30 @@ export default ({ data, pageContext }) => {
         >
           Gatsby Couture Blog
         </h1>
-        <h4>{}Posts</h4>
+        <h4>{data.allMarkdownRemark.totalCount}Posts</h4>
+        {
+          data.allMarkdownRemark.edges.map(({ node }) => (
+            <div key={node.id}>
+              <h3>
+              
+              </h3>
+              <p>{node.excerpt}</p>
+            </div>
+          ))
+        }
+
+        {/*Links for pagination */}
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            maxWidth: 300,
+            margin: '0 auto',
+          }}
+        >
         
+        </div>
       </div>
     </Layout>
   )
@@ -29,7 +51,22 @@ export default ({ data, pageContext }) => {
 export const query = graphql `
   ($skip: Int!, $limit: Int!) {
     allMarkdownRemark (
-
-    )
+      skip: $skip
+      limit: $limit
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      totalCount
+      edges {
+        fields {
+          slug
+        }
+        id
+        frontmatter {
+          title
+          date(formatString: "MMMM Do, YYYY")
+        }
+        excerpt
+      }
+    }
   }
 `
