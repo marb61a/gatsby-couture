73d77f77bbc7443b0e2dfa427a797df61d7fef5e
marb61a <marb61a@gmail.com>
2019-03-18 20:26:52 +0000

    added table to page-3.js

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -24,7 +24,30 @@ export default () => (
     <h3>Image File Data</h3>
     <StaticQuery
       query={getImageData}
-      render={}
+      render={data => (
+        <table>
+          <thead>
+            <tr>
+              <th>Relative Path</th>
+              <th>Size of Image</th>
+              <th>Extension</th>
+              <th>Birthtime</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.allFile.edges.map(({ node }, index) => (
+                <tr key={index}>
+                  <td>{node.relativePath}</td>
+                  <td>{node.size}</td>
+                  <td>{node.extension}</td>
+                  <td>{node.birthTime}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      )}
     />
     <Link to="/page-2">Go to page 2</Link>
   </Layout>
