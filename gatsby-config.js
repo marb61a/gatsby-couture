382848d2193fe55242f68a949769add174ebc0cd
marb61a <marb61a@gmail.com>
2019-03-21 11:50:08 +0000

    updated gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: `Gatsby Couture`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `Martin`,
+    createdAt: 2019
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,6 +12,23 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -25,6 +43,20 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-snipcart',
+      options: {
+        apiKey: process.env.SNIPCART_API_KEY,
+        autopop: true,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- gatsby-node.js
+++ gatsby-node.js
@@ -8,4 +8,13 @@ const ProductTemplate = path.resolve('./src/templates/product-template.js')
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   
+  if(node.internal.type === "MarkdownRemark") {
+    const slug = createFilePath({ node, getNode, basePath: 'posts' })
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug
+    })
+  }
 }
+
