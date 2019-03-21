c4012db220e00c1607c04feede13d0159ea6e7d6
marb61a <marb61a@gmail.com>
2019-03-21 12:04:24 +0000

    updated gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,3 +18,39 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 }
 
+exports.createPages = async({ graphql, actions }) => {
+  const { createPage } = actions 
+  const result = await graphql(`
+    {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+
+      allContentfulProduct {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+}
+
+const posts = result.data.allMarkdownRemark.edges
+posts.forEach(({ node: post }) => {
+  createPage({
+    path: `posts${post.fields.slug}`,
+    component: PostTemplate,
+    context: {
+      slug: post.fields.slug
+    }
+  })
+})
+
