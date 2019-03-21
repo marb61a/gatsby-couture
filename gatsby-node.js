907b863881cc6d138dd62167b6cc76a90abfa1bc
marb61a <marb61a@gmail.com>
2019-03-21 12:31:52 +0000

    updated gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -54,3 +54,35 @@ posts.forEach(({ node: post }) => {
   })
 })
 
+const postsPerPage = 2
+const totalPages = Math.ceil(posts.length / postsPerPage)
+Array.from({ length: totalPages })
+  .forEach((_, index) => {
+    const currentPage = index + 1
+    const isFirstPage = index === 0
+    const isLastPage = currentPage === totalPages
+    
+    createPages({
+      path: isFirstPage ? '/blog' : `/blog/${currentPage}`,
+      component: BlogTemplate,
+      context: {
+        limit: postsPerPage,
+        skip: index * postsPerPage,
+        isFirstPage,
+        isLastPage,
+        currentPage,
+        totalPages
+      }
+    })
+  })
+
+  const products = result.data.allContentfulProduct.edges
+  products.forEach(({ node: product }) => {
+    createPage({
+      path: `/products/${product.slug}`,
+      component: ProductTemplate,
+      context: {
+        slug: product.slug
+      }
+    })
+  })
